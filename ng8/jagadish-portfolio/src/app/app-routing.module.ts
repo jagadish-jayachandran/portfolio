3ffb35bdb814bd92aacc72c7fbb5a855f4731906
jagadish pj <jagadishsquaredotssolutions@gmail.com>
2019-12-28 09:23:05 +0530

    components added

--- ng8/jagadish-portfolio/src/app/app-routing.module.ts
+++ ng8/jagadish-portfolio/src/app/app-routing.module.ts
@@ -2,7 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    redirectTo: '/profile',
+    pathMatch: 'full'
+  },
+  { 
+    path: 'profile', 
+  loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule) 
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ng8/jagadish-portfolio/src/app/app.module.ts
+++ ng8/jagadish-portfolio/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
